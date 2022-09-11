3e82524dd254095668dd8f424f7e28f1bcb425cc
bcaudillo <btcaudillo@gmail.com>
2022-09-11 11:17:35 -0600

    commit tree 83155e1fbc524bc8d7f9b4ad6be42fe19f2355f6

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
+// console.log(name)
+// console.log(about)
+// console.log(technologies)
+// name = name.map(item=>item.name)
+return (
+  <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {technologies.map((item, index) => <span key ={index}>{item}</span>)}
+      
+
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,23 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  // let name = projects.map(item => item.name)
+  // let about = projects.map(item => item.about)
+  // let technologies = projects.map(item=>item.technologies)
+  // let itTechnologies = technologies.map(item => item)
+  // console.log(itTechnologies[0][0])
+  //projects.forEach(item =>console.log((item.technologies).forEach(item =>console.log(item))))
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map(item => <ProjectItem key ={item.id} name = {item.name} about ={item.about} technologies ={item.technologies}/>)}</div>
+      {/* name = {name[0]} about = {about[0]} technologies = {itTechnologies[0]}
+      name = {name[1]} about = {about[1]} technologies = {itTechnologies[1]}
+      name = {name[2]} about = {about[2]} technologies = {itTechnologies[2]} */}
+
     </div>
-  );
+  ); 
 }
 
 export default ProjectList;
+ 
